090624625d6510a29e4f1c960decda94e3812a60
zoukuan <luxiaojian2013@gmail.com>
2019-01-21 20:01:50 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'add .'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:lawrencelulu/acm.git master:master
+git push -f git@github.com:lawrencelulu/acm.git master:gh-pages
 
 cd -
